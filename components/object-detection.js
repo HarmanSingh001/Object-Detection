840aef6c9b8b47dc174edc86ef3a0662b1328c5e
Harman Singh <harmansingh2670@gmail.com>
2024-12-25 00:50:59 +0530

    first update

---
+++ components/object-detection.js
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+
+const ObjectDetection = () => {
+    const webcamRef = useRef(null);
+
+    const showmyVideo = () => {
+        if (
+            webcamRef.current !== null &&
+            webcamRef.current.video?.readyState === 4
+        ) {
+            const myVideoWidth = webcamRef.current.video.videoWidth;
+            const myVideoHeight = webcamRef.current.video.videoHeight;
+
+            webcamRef.current.video.width = myVideoWidth;
+            webcamRef.current.video.heigth = myVideoHeight;
+        }
+    };
+    useEffect(() => {
+        showmyVideo();
+    });
+    return (
+        <div className="mt-8">
+            <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+                {/* webcam */}
+                <Webcam
+                    useRef={webcamRef}
+                    className="rounded-md w-full lg:h-[720px]"
+                    muted
+                />
+                {/* canvas */}
+            </div>
+        </div>
+    );
+};
+
+export default ObjectDetection;
